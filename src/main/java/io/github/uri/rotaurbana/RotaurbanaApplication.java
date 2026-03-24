91428dc8966112e71358bfda884000619bfddd9c
Otávio Kappann <kappannotavio@gmail.com>
2026-03-24 14:53:09 -0300

    feat: initial commit

---
+++ src/main/java/io/github/uri/rotaurbana/RotaurbanaApplication.java
@@ -0,0 +1,13 @@
+package io.github.uri.rotaurbana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaurbanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaurbanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/uri/rotaurbana/RotaurbanaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.uri.rotaurbana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaurbanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
