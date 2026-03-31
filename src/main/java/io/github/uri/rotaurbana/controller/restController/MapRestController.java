51db8873868fdbed6a7a2295d1e60d5c060444b1
Otávio Gonçalves Kappann dos Santos <kappannotavio@gmail.com>
2026-03-31 15:34:42 -0300

    feat: adding get and post requisitions for map location

--- src/main/java/io/github/uri/rotaurbana/controller/restController/MapRestController.java
+++ src/main/java/io/github/uri/rotaurbana/controller/restController/MapRestController.java
@@ -1,7 +1,27 @@
 package io.github.uri.rotaurbana.controller.restController;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.github.uri.rotaurbana.dto.LocationDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/map")
 public class MapRestController {
+
+    Map<String, LocationDTO> busLocations = new HashMap<>();
+
+    //Recebe do Front
+    @PostMapping("/{busId}")
+    public void receiveLocation(@PathVariable String busId, @RequestBody LocationDTO dto) {
+        busLocations.put(busId, dto);
+    }
+
+    //Manda pro Front
+    @GetMapping("/{busId}")
+    public LocationDTO getLocation(@PathVariable String busId) {
+        return busLocations.get(busId);
+    }
+
 }
