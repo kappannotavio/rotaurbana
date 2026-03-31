611536430cd971722d15a5bb1cac9b84d4b31ebf
Otávio Gonçalves Kappann dos Santos <kappannotavio@gmail.com>
2026-03-31 15:31:40 -0300

    feat: adding LocationDTO

---
+++ src/main/java/io/github/uri/rotaurbana/dto/LocationDTO.java
@@ -0,0 +1,28 @@
+package io.github.uri.rotaurbana.dto;
+
+public class LocationDTO {
+
+    private double lat;
+    private double lon;
+
+
+    //methods
+
+
+    //getters and setters
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
