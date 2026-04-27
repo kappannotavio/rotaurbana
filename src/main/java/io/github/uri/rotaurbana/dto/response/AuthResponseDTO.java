9d9832a489feb15b83b5ad2d517b03d8fc36e4a0
Otávio Gonçalves Kappann dos Santos <kappannotavio@gmail.com>
2026-04-27 20:52:12 -0300

    refactor: changing DTO names

---
+++ src/main/java/io/github/uri/rotaurbana/dto/response/AuthResponseDTO.java
@@ -0,0 +1,7 @@
+package io.github.uri.rotaurbana.dto.response;
+
+public record AuthResponseDTO(
+        String email,
+        String password
+) {
+}
---
+++ src/main/java/io/github/uri/rotaurbana/dto/response/LoginResponseDTO.java
@@ -0,0 +1,6 @@
+package io.github.uri.rotaurbana.dto.response;
+
+public record LoginResponseDTO(
+        String token
+){
+}
