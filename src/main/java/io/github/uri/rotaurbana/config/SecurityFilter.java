7665238c51aab9de47f8552ec5b53f6a44447a6a
Otávio <kappannotavio@gmail.com>
2026-05-24 20:37:02 -0300

    refactor: adding more auth configs

--- src/main/java/io/github/uri/rotaurbana/config/SecurityFilter.java
+++ src/main/java/io/github/uri/rotaurbana/config/SecurityFilter.java
@@ -36,7 +36,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null){
-            var email = tokenService.validadeToken(token);
+            var email = tokenService.validateToken(token);
             if (email != null && !email.isEmpty()) {
                 UserDetails userDetails = userRepository.findByEmail(email);
                 if (userDetails != null) {
