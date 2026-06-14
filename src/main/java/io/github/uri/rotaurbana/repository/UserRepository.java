816e85e16c2a6e0d13f42efd3471ebf5f03614d1
Otávio <kappannotavio@gmail.com>
2026-06-14 15:49:11 -0300

    refactor: improve email search

--- src/main/java/io/github/uri/rotaurbana/repository/UserRepository.java
+++ src/main/java/io/github/uri/rotaurbana/repository/UserRepository.java
@@ -3,13 +3,12 @@ package io.github.uri.rotaurbana.repository;
 import io.github.uri.rotaurbana.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     @Query("SELECT u FROM UserEntity u WHERE LOWER(u.email) = LOWER(?1)")
-    UserDetails findByEmail(String email);
+    UserEntity findByEmail(String email);
 
 }
